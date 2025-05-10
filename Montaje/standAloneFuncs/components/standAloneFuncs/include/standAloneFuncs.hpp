4fbc51de70a52f9b8ae2bb6fbabc24708decb373
santa <manuelsanta06@gmail.com>
2025-05-10 19:20:38 -0300

    basic functions and macros

--- Montaje/standAloneFuncs/components/standAloneFuncs/include/standAloneFuncs.hpp
+++ Montaje/standAloneFuncs/components/standAloneFuncs/include/standAloneFuncs.hpp
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "hal/gpio_types.h"         //para los enums de mierdas de los GPIO que obliga a usar
+
+//defines numericos
+#define HIGH 1
+#define LOW 0
+
+
+//defines funcionales
+#define delayMs(ms) (vTaskDelay( ms / portTICK_PERIOD_MS))
+#define dWrite(pin,level) (gpio_set_level(static_cast<gpio_num_t>(pin), level))
+#define dRead(pin) (gpio_get_level(pin)
+
+//funciones
+void adc_init(void);
