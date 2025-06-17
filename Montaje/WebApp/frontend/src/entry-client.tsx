87467e0d96c17f94da9f3b2f788a4f5f947e3cdf
santa <manuelsanta06@gmail.com>
2025-06-17 09:44:53 -0300

    react bootstrap added

--- Montaje/WebApp/frontend/src/entry-client.tsx
+++ Montaje/WebApp/frontend/src/entry-client.tsx
@@ -1,12 +1,8 @@
-import "./index.css";
 import App from "./App";
-import { StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
 
 hydrateRoot(
   document.getElementById("root") as HTMLElement,
-  <StrictMode>
       <App />
-  </StrictMode>
 );
--- Montaje/WebApp/frontend/src/entry-server.tsx
+++ Montaje/WebApp/frontend/src/entry-server.tsx
@@ -1,13 +1,10 @@
 import App from "./App";
-import { StrictMode } from "react";
 import { renderToString } from "react-dom/server";
 
 
 export function render(_url: string) {
     const html = renderToString(
-        <StrictMode>
             <App />
-        </StrictMode>
     );
     return { html };
 }
