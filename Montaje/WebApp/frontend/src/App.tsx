5e3562127601f61f3b0f7ce712f53e079f9767e4
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-06-19 13:00:07 -0300

    trabajo desarrollo encuestas

--- Montaje/WebApp/frontend/src/App.tsx
+++ Montaje/WebApp/frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
-import { useState } from "react";
-
 
 function App() {
     return (
